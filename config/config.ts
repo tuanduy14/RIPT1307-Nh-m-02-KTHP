1f383966bd4a15776b62ef5db8d62550bb522a97
tuanduy14 <duy15101996@gmail.com>
2026-05-31 18:17:31 +0700

    test

--- config/config.ts
+++ config/config.ts
@@ -1,11 +1,19 @@
 // https://umijs.org/config/
-import { defineConfig } from 'umi';
+import { defineConfig, history } from 'umi';
 import defaultSettings from './defaultSettings';
 import routes from './routes';
+import React from 'react';
+import Footer from '../src/components/Footer';
+import RightContent from '../src/components/RightContent';
+import { OIDCBounder } from '../src/components/OIDCBounder';
+import { unCheckPermissionPaths } from '../src/components/OIDCBounder/constant';
+import TechnicalSupportBounder from '../src/components/TechnicalSupportBounder';
+import { currentRole } from '../src/utils/ip';
 // import proxy from './proxy';
 // const { REACT_APP_ENV } = process.env;
 
 export default defineConfig({
+	presets: ['@umijs/preset-react'],
 	hash: true,
 	antd: {},
 	dva: {
@@ -15,6 +23,50 @@ export default defineConfig({
 		// https://umijs.org/zh-CN/plugins/plugin-layout
 		locale: true,
 		...defaultSettings,
+		unAccessible: React.createElement(
+			OIDCBounder,
+			null,
+			React.createElement(
+				TechnicalSupportBounder,
+				null,
+				React.createElement('div', null, '403 - Không có quyền truy cập'),
+			),
+		),
+		noFound: React.createElement('div', null, '404 - Không tìm thấy trang'),
+		rightContentRender: () => React.createElement(RightContent, null),
+		disableContentMargin: false,
+		footerRender: () => React.createElement(Footer, null),
+		onPageChange: () => {
+			const { location } = history;
+			const isUncheckPath = unCheckPermissionPaths.some((path) => window.location.pathname.includes(path));
+			const initialState = (window as any).g_initialState;
+
+			if (location.pathname === '/') {
+				history.replace('/dashboard');
+			} else if (
+				!isUncheckPath &&
+				currentRole &&
+				initialState?.authorizedPermissions?.length &&
+				!initialState?.authorizedPermissions?.find((item: any) => item.rsname === currentRole)
+			) {
+				history.replace('/403');
+			}
+		},
+		menuItemRender: (item: any, dom: any) => React.createElement(
+			'a',
+			{
+				className: 'not-underline',
+				key: item?.path,
+				href: item?.path,
+				onClick: (e: React.MouseEvent<HTMLAnchorElement>) => {
+					e.preventDefault();
+					history.push(item?.path ?? '/');
+				},
+				style: { display: 'block' },
+			},
+			dom,
+		),
+		menuHeaderRender: undefined,
 	},
 	// https://umijs.org/zh-CN/plugins/plugin-locale
 	locale: {
--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,14 @@
-import Footer from '@/components/Footer';
-import RightContent from '@/components/RightContent';
+import type { ReactNode } from 'react';
 import { notification } from 'antd';
 import 'moment/locale/vi';
-import type { RequestConfig, RunTimeLayoutConfig } from 'umi';
-import { getIntl, getLocale, history } from 'umi';
+import type { RequestConfig } from 'umi';
+import { getIntl, getLocale } from 'umi';
 import type { RequestOptionsInit, ResponseError } from 'umi-request';
 import ErrorBoundary from './components/ErrorBoundary';
 import { OIDCBounder } from './components/OIDCBounder';
-import { unCheckPermissionPaths } from './components/OIDCBounder/constant';
 import OneSignalBounder from './components/OneSignalBounder';
-import TechnicalSupportBounder from './components/TechnicalSupportBounder';
 import type { IInitialState } from './services/base/typing';
 import './styles/global.less';
-import { currentRole } from './utils/ip';
 
 export const initialStateConfig = {
 	loading: <></>,
@@ -25,6 +21,16 @@ export async function getInitialState(): Promise<IInitialState> {
 }
 
 const authHeaderInterceptor = (url: string, options: RequestOptionsInit) => ({});
+
+export function rootContainer(container: ReactNode) {
+	return (
+		<OIDCBounder>
+			<ErrorBoundary>
+				<OneSignalBounder>{container}</OneSignalBounder>
+			</ErrorBoundary>
+		</OIDCBounder>
+	);
+}
 
 export const request: RequestConfig = {
 	errorHandler: (error: ResponseError) => {
@@ -53,60 +59,3 @@ export const request: RequestConfig = {
 	requestInterceptors: [authHeaderInterceptor],
 };
 
-export const layout: RunTimeLayoutConfig = ({ initialState }) => {
-	return {
-		unAccessible: (
-			<OIDCBounder>
-				<TechnicalSupportBounder>
-					<div>403 - Không có quyền truy cập</div>
-				</TechnicalSupportBounder>
-			</OIDCBounder>
-		),
-		noFound: <div>404 - Không tìm thấy trang</div>,
-		rightContentRender: () => <RightContent />,
-		disableContentMargin: false,
-		footerRender: () => <Footer />,
-
-		onPageChange: () => {
-			if (initialState?.currentUser) {
-				const { location } = history;
-				const isUncheckPath = unCheckPermissionPaths.some((path) => window.location.pathname.includes(path));
-
-				if (location.pathname === '/') {
-					history.replace('/dashboard');
-				} else if (
-					!isUncheckPath &&
-					currentRole &&
-					initialState?.authorizedPermissions?.length &&
-					!initialState?.authorizedPermissions?.find((item) => item.rsname === currentRole)
-				)
-					history.replace('/403');
-			}
-		},
-
-		menuItemRender: (item: any, dom: any) => (
-			<a
-				className='not-underline'
-				key={item?.path}
-				href={item?.path}
-				onClick={(e) => {
-					e.preventDefault();
-					history.push(item?.path ?? '/');
-				}}
-				style={{ display: 'block' }}
-			>
-				{dom}
-			</a>
-		),
-
-		childrenRender: (dom: any) => (
-			<OIDCBounder>
-				<ErrorBoundary>
-					<OneSignalBounder>{dom}</OneSignalBounder>
-				</ErrorBoundary>
-			</OIDCBounder>
-		),
-		menuHeaderRender: undefined,
-		...initialState?.settings,
-	};
-};
