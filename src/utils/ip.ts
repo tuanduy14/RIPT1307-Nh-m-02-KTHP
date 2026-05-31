534ddff9177af4bac4a30ed3743a2a1f9fd0ff5c
tuanduy14 <duy15101996@gmail.com>
2026-05-31 16:15:28 +0700

    Fix: clear all remaining app configs in ip.ts

--- src/utils/ip.ts
+++ src/utils/ip.ts
@@ -1,39 +1,40 @@
 import { AppModules, EModuleKey } from '@/services/base/constant';
 
-const ipRoot = APP_CONFIG_IP_ROOT; // ip dev
+// Điền chính xác link Render Backend của bạn vào đây (nhớ giữ dấu nháy đơn)
+const ipRoot = 'https://club-backend-dmy7.onrender.com/api/'; 
 
 // Ip Chính => Mặc định dùng trong các useInitModel
-const ip3 = ipRoot + 'slink'; // ip dev
+const ip3 = ipRoot + 'slink'; 
 
 // Ip khác
-const ipNotif = ipRoot + 'notification'; // ip dev
-const ipSlink = ipRoot + 'slink'; // ip dev
+const ipNotif = ipRoot + 'notification'; 
+const ipSlink = ipRoot + 'slink'; 
 
 const currentRole = EModuleKey.CONNECT;
 const oneSignalRole = EModuleKey.CONNECT;
 
 // DO NOT TOUCH
 const keycloakClientID = AppModules[currentRole].clientId;
-const keycloakAuthority = APP_CONFIG_KEYCLOAK_AUTHORITY;
-const resourceServerClientId = `${APP_CONFIG_PREFIX_OF_KEYCLOAK_CLIENT_ID}auth`;
-const keycloakAuthEndpoint = APP_CONFIG_KEYCLOAK_AUTHORITY + '/protocol/openid-connect/auth';
-const keycloakTokenEndpoint = APP_CONFIG_KEYCLOAK_AUTHORITY + '/protocol/openid-connect/token';
-const keycloakUserInfoEndpoint = APP_CONFIG_KEYCLOAK_AUTHORITY + '/protocol/openid-connect/userinfo';
-const sentryDSN = APP_CONFIG_SENTRY_DSN;
-const oneSignalClient = APP_CONFIG_ONE_SIGNAL_ID;
+const keycloakAuthority = 'APP_CONFIG_KEYCLOAK_AUTHORITY';
+const resourceServerClientId = 'auth_resource_client_id';
+const keycloakAuthEndpoint = 'APP_CONFIG_KEYCLOAK_AUTHORITY/protocol/openid-connect/auth';
+const keycloakTokenEndpoint = 'APP_CONFIG_KEYCLOAK_AUTHORITY/protocol/openid-connect/token';
+const keycloakUserInfoEndpoint = 'APP_CONFIG_KEYCLOAK_AUTHORITY/protocol/openid-connect/userinfo';
+const sentryDSN = 'APP_CONFIG_SENTRY_DSN';
+const oneSignalClient = 'APP_CONFIG_ONE_SIGNAL_ID';
 
 export {
-	ip3,
-	ipNotif,
-	ipSlink,
-	currentRole,
-	oneSignalRole,
-	keycloakClientID,
-	resourceServerClientId,
-	keycloakAuthEndpoint,
-	keycloakTokenEndpoint,
-	keycloakUserInfoEndpoint,
-	keycloakAuthority,
-	sentryDSN,
-	oneSignalClient,
+    ip3,
+    ipNotif,
+    ipSlink,
+    currentRole,
+    oneSignalRole,
+    keycloakClientID,
+    resourceServerClientId,
+    keycloakAuthEndpoint,
+    keycloakTokenEndpoint,
+    keycloakUserInfoEndpoint,
+    keycloakAuthority,
+    sentryDSN,
+    oneSignalClient,
 };
