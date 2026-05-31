b6411a7689bd8ed39c07d51fd5d523e66f1311ba
tuanduy14 <duy15101996@gmail.com>
2026-05-31 16:32:55 +0700

    Fix: change layout noFound to function render

--- src/app.tsx
+++ src/app.tsx
@@ -19,7 +19,7 @@ import { currentRole } from './utils/ip';
 
 /**  loading */
 export const initialStateConfig = {
-	loading: <></>,
+    loading: <></>,
 };
 
 /**
@@ -27,9 +27,9 @@ export const initialStateConfig = {
  * // Tobe removed
  * */
 export async function getInitialState(): Promise<IInitialState> {
-	return {
-		permissionLoading: true,
-	};
+    return {
+        permissionLoading: true,
+    };
 }
 
 // Tobe removed
@@ -39,90 +39,91 @@ const authHeaderInterceptor = (url: string, options: RequestOptionsInit) => ({})
  * @see https://beta-pro.ant.design/docs/request-cn
  */
 export const request: RequestConfig = {
-	errorHandler: (error: ResponseError) => {
-		const { messages } = getIntl(getLocale());
-		const { response } = error;
+    errorHandler: (error: ResponseError) => {
+        const { messages } = getIntl(getLocale());
+        const { response } = error;
 
-		if (response && response.status) {
-			const { status, statusText, url } = response;
-			const requestErrorMessage = messages['app.request.error'];
-			const errorMessage = `${requestErrorMessage} ${status}: ${url}`;
-			const errorDescription = messages[`app.request.${status}`] || statusText;
-			notification.error({
-				message: errorMessage,
-				description: errorDescription,
-			});
-		}
+        if (response && response.status) {
+            const { status, statusText, url } = response;
+            const requestErrorMessage = messages['app.request.error'];
+            const errorMessage = `${requestErrorMessage} ${status}: ${url}`;
+            const errorDescription = messages[`app.request.${status}`] || statusText;
+            notification.error({
+                message: errorMessage,
+                description: errorDescription,
+            });
+        }
 
-		if (!response) {
-			notification.error({
-				description: 'Yêu cầu gặp lỗi',
-				message: 'Bạn hãy thử lại sau',
-			});
-		}
-		throw error;
-	},
-	requestInterceptors: [authHeaderInterceptor],
+        if (!response) {
+            notification.error({
+                description: 'Yêu cầu gặp lỗi',
+                message: 'Bạn hãy thử lại sau',
+            });
+        }
+        throw error;
+    },
+    requestInterceptors: [authHeaderInterceptor],
 };
 
 // ProLayout  https://procomponents.ant.design/components/layout
 export const layout: RunTimeLayoutConfig = ({ initialState }) => {
-	return {
-		unAccessible: (
-			<OIDCBounder>
-				<TechnicalSupportBounder>
-					<NotAccessible />
-				</TechnicalSupportBounder>
-			</OIDCBounder>
-		),
-		noFound: <NotFoundContent />,
-		rightContentRender: () => <RightContent />,
-		disableContentMargin: false,
+    return {
+        unAccessible: (
+            <OIDCBounder>
+                <TechnicalSupportBounder>
+                    <NotAccessible />
+                </TechnicalSupportBounder>
+            </OIDCBounder>
+        ),
+        // SỬA TẠI ĐÂY: Chuyển từ Component sang dạng Function Render để tránh lỗi sập Layout plugin
+        noFound: () => <NotFoundContent />,
+        rightContentRender: () => <RightContent />,
+        disableContentMargin: false,
 
-		footerRender: () => <Footer />,
+        footerRender: () => <Footer />,
 
-		onPageChange: () => {
-			if (initialState?.currentUser) {
-				const { location } = history;
-				const isUncheckPath = unCheckPermissionPaths.some((path) => window.location.pathname.includes(path));
+        onPageChange: () => {
+            if (initialState?.currentUser) {
+                const { location } = history;
+                const isUncheckPath = unCheckPermissionPaths.some((path) => window.location.pathname.includes(path));
 
-				if (location.pathname === '/') {
-					history.replace('/student/dashboard');
-				} else if (
-					!isUncheckPath &&
-					currentRole &&
-					initialState?.authorizedPermissions?.length &&
-					!initialState?.authorizedPermissions?.find((item) => item.rsname === currentRole)
-				)
-					history.replace('/403');
-			}
-		},
+                if (location.pathname === '/') {
+                    history.replace('/student/dashboard');
+                } else if (
+                    !isUncheckPath &&
+                    currentRole &&
+                    initialState?.authorizedPermissions?.length &&
+                    !initialState?.authorizedPermissions?.find((item) => item.rsname === currentRole)
+                )
+                    history.replace('/403');
+            }
+        },
 
-		menuItemRender: (item: any, dom: any) => (
-			<a
-				className='not-underline'
-				key={item?.path}
-				href={item?.path}
-				onClick={(e) => {
-					e.preventDefault();
-					history.push(item?.path ?? '/');
-				}}
-				style={{ display: 'block' }}
-			>
-				{dom}
-			</a>
-		),
+        menuItemRender: (item: any, dom: any) => (
+            <a
+                className='not-underline'
+                key={item?.path}
+                href={item?.path}
+                onClick={(e) => {
+                    e.preventDefault();
+                    history.push(item?.path ?? '/');
+                }}
+                style={{ display: 'block' }}
+            >
+                {dom}
+            </a>
+        ),
 
-		childrenRender: (dom) => (
-			<OIDCBounder>
-				<ErrorBoundary>
-					{/* <TechnicalSupportBounder> */}
-					<OneSignalBounder>{dom}</OneSignalBounder>
-					{/* </TechnicalSupportBounder> */}
-				</ErrorBoundary>
-			</OIDCBounder>
-		),
-		menuHeaderRender: undefined,
-		...initialState?.settings,
-	};
+        childrenRender: (dom) => (
+            <OIDCBounder>
+                <ErrorBoundary>
+                    {/* <TechnicalSupportBounder> */}
+                    <OneSignalBounder>{dom}</OneSignalBounder>
+                    {/* </TechnicalSupportBounder> */}
+                </ErrorBoundary>
+            </OIDCBounder>
+        ),
+        menuHeaderRender: undefined,
+        ...initialState?.settings,
+    };
 };
