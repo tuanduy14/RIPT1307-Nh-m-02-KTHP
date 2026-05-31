04dcd0d949112227f6d716ddcb3882bc2cf8730f
tuanduy14 <duy15101996@gmail.com>
2026-05-31 15:52:46 +0700

    Fix: add quotes to app configs

--- src/services/base/constant.ts
+++ src/services/base/constant.ts
@@ -16,67 +16,67 @@ export enum EModuleKey {
 
 export const AppModules: Record<EModuleKey, Login.TModule> = {
 	[EModuleKey.CONNECT]: {
-		title: APP_CONFIG_TITLE_CONNECT,
+		title: 'APP_CONFIG_TITLE_CONNECT',
 		clientId: `${APP_CONFIG_PREFIX_OF_KEYCLOAK_CLIENT_ID}connect`,
 		url: APP_CONFIG_URL_CONNECT,
 		icon: EModuleKey.CONNECT + '.svg',
 	},
 	[EModuleKey.CONG_CAN_BO]: {
-		title: APP_CONFIG_TITLE_CAN_BO,
+		title: 'APP_CONFIG_TITLE_CAN_BO',
 		clientId: `${APP_CONFIG_PREFIX_OF_KEYCLOAK_CLIENT_ID}connect`,
 		url: APP_CONFIG_URL_CAN_BO,
 		icon: EModuleKey.CONG_CAN_BO + '.svg',
 	},
 	[EModuleKey.CORE]: {
-		title: APP_CONFIG_TITLE_CORE,
+		title: 'APP_CONFIG_TITLE_CORE',
 		clientId: `${APP_CONFIG_PREFIX_OF_KEYCLOAK_CLIENT_ID}core`,
 		url: APP_CONFIG_URL_CORE,
 		icon: EModuleKey.CORE + '.svg',
 	},
 	[EModuleKey.QLDT]: {
-		title: APP_CONFIG_TITLE_DAO_TAO,
+		title: 'APP_CONFIG_TITLE_DAO_TAO',
 		clientId: `${APP_CONFIG_PREFIX_OF_KEYCLOAK_CLIENT_ID}qldt`,
 		url: APP_CONFIG_URL_DAO_TAO,
 		icon: EModuleKey.QLDT + '.svg',
 	},
 	[EModuleKey.TCNS]: {
-		title: APP_CONFIG_TITLE_NHAN_SU,
+		title: 'APP_CONFIG_TITLE_NHAN_SU',
 		clientId: `${APP_CONFIG_PREFIX_OF_KEYCLOAK_CLIENT_ID}tcns`,
 		url: APP_CONFIG_URL_NHAN_SU,
 		icon: EModuleKey.TCNS + '.svg',
 	},
 	[EModuleKey.CTSV]: {
-		title: APP_CONFIG_TITLE_CTSV,
+		title: 'APP_CONFIG_TITLE_CTSV',
 		clientId: `${APP_CONFIG_PREFIX_OF_KEYCLOAK_CLIENT_ID}ctsv`,
 		url: APP_CONFIG_URL_CTSV,
 		icon: EModuleKey.CTSV + '.svg',
 	},
 	[EModuleKey.VPS]: {
-		title: APP_CONFIG_TITLE_VPS,
+		title: 'APP_CONFIG_TITLE_VPS',
 		clientId: `${APP_CONFIG_PREFIX_OF_KEYCLOAK_CLIENT_ID}vps`,
 		url: APP_CONFIG_URL_VPS,
 		icon: EModuleKey.VPS + '.svg',
 	},
 	[EModuleKey.QLKH]: {
-		title: APP_CONFIG_TITLE_QLKH,
+		title: 'APP_CONFIG_TITLE_QLKH',
 		clientId: `${APP_CONFIG_PREFIX_OF_KEYCLOAK_CLIENT_ID}qlkh`,
 		url: APP_CONFIG_URL_QLKH,
 		icon: EModuleKey.QLKH + '.svg',
 	},
 	[EModuleKey.TC]: {
-		title: APP_CONFIG_TITLE_TAI_CHINH,
+		title: 'APP_CONFIG_TITLE_TAI_CHINH',
 		clientId: `${APP_CONFIG_PREFIX_OF_KEYCLOAK_CLIENT_ID}tc`,
 		url: APP_CONFIG_URL_TAI_CHINH,
 		icon: EModuleKey.TC + '.svg',
 	},
 	[EModuleKey.KT]: {
-		title: APP_CONFIG_TITLE_KHAO_THI,
+		title: 'APP_CONFIG_TITLE_KHAO_THI',
 		clientId: `${APP_CONFIG_PREFIX_OF_KEYCLOAK_CLIENT_ID}kt`,
 		url: APP_CONFIG_URL_KHAO_THI,
 		icon: EModuleKey.KT + '.svg',
 	},
 	[EModuleKey.CSVC]: {
-		title: APP_CONFIG_TITLE_CSVC,
+		title: 'APP_CONFIG_TITLE_CSVC',
 		clientId: `${APP_CONFIG_PREFIX_OF_KEYCLOAK_CLIENT_ID}csvc`,
 		url: APP_CONFIG_URL_CSVC,
 		icon: EModuleKey.CSVC + '.svg',
@@ -84,19 +84,19 @@ export const AppModules: Record<EModuleKey, Login.TModule> = {
 };
 
 export const moduleThuVien: Partial<Login.TModule> = {
-	title: APP_CONFIG_TITLE_THU_VIEN,
+	title: 'APP_CONFIG_TITLE_THU_VIEN',
 	url: APP_CONFIG_URL_THU_VIEN,
 	icon: 'thu-vien.svg',
 };
 
 export const moduleQuanLyVanBan: Partial<Login.TModule> = {
-	title: APP_CONFIG_TITLE_QLVB,
+	title: 'APP_CONFIG_TITLE_QLVB',
 	url: APP_CONFIG_URL_QLVB,
 	icon: 'quan-ly-van-ban.svg',
 };
 
 export const moduleCongThongTin: Partial<Login.TModule> = {
-	title: APP_CONFIG_TITLE_LANDING,
+	title: 'APP_CONFIG_TITLE_LANDING',
 	url: APP_CONFIG_URL_LANDING,
 	icon: 'cong-thong-tin.svg',
 };
